afe0790fb9d6a1ef36ae6796df2eff328d73cc5f
EvanSink <Evan.Sinkfield4728@gmail.com>
2025-04-15 23:40:50 +0000

    added account deletion (doesnt work) and exiting the program early (meaningless for now)
    and comments ig

--- initialize_db.py
+++ initialize_db.py
@@ -2,7 +2,7 @@ import sqlite3
 
 DB_NAME = 'evans_project.db'
 
-
+"""Initializes the database."""
 def initialize_database():
     connection = sqlite3.connect(DB_NAME)
     print("Connected to the database.")
@@ -20,23 +20,53 @@ def initialize_database():
     ''')
 
     print("Table created.")
+    """Asks to delete an existing account. DOES NOT WORK!!!"""
+    delete_existing_data = input("Do you want to delete an existing account? (yes/no): ")
+    if delete_existing_data.lower() == 'yes':
+        username_to_delete = input("Enter the username of the account to delete: ")
+        cursor.execute('DELETE FROM bankingInfo WHERE username = ?', (username_to_delete,))
+        print(f"Account with username '{username_to_delete}' deleted.")
+        print("THIS FEATURE ONLY WORKS WITH SQL PRO! VERY SORRY!")
+    else:
+        print("No accounts deleted.")
+        print("THIS FEATURE ONLY WORKS WITH SQL PRO! VERY SORRY!")
+    """Asks the user if they would like to exit the program or continue."""
+    ask_to_exit = input("Is there anything else you want to do? (yes/no): ")
+    if ask_to_exit.lower() == 'yes':
+        print("Returning to the program...")
+    else:
+        print("Exiting the program...")
+        exit()
+    
+    
 
+    """Asks for the user and their info to make a new account."""
     print("Please enter the following information:")
     username = input("Username: ")
     bank_pin = int(input("Bank Pin: "))
     credit = int(input("Credit: "))
     balance = int(input("Balance: "))
 
+    """Creates new account."""
     # Insert sample data
     print("Inserting sample data...")
     cursor.execute('''
         INSERT INTO bankingInfo (username, bank_pin, credit, balance) VALUES
         (?, ?, ?, ?)
-    ''', (username, bank_pin, credit, balance))
+    ''', (username.lower(), bank_pin, credit, balance))
     print("Person data inserted.")
     # Commit the changes and close the connection
     print("Making sure the code is actually changing and not just repeating...")
+    """
+    maybe_delete = input("Do you want to delete the account? (yes/no): ")
+    if maybe_delete.lower() == 'yes':
+        cursor.execute('DELETE FROM bankingInfo WHERE username = ?', (username,))
+        print("Account deleted.")
+    else:
+        print("Account not deleted.")
+    """
     print("Committing changes and closing the connection...")
+    print("Thank you!")
     connection.commit()
     connection.close()
 
--- test.py
+++ test.py
@@ -1,18 +1,15 @@
 import unittest
-from num_operations import is_odd
-from num_operations import is_even
+from initialize_db import initialize_database
+
 
 class TestNumbersOperations(unittest.TestCase):
-    def test_is_odd(self):
-        self.assertEqual(is_odd(3), True)
-        self.assertEqual(is_odd(4), False)
-        self.assertEqual(is_odd(0), False)
-        self.assertEqual(is_odd(-1), True)
-    def test_is_even(self):
-        self.assertEqual(is_even(3), False)
-        self.assertEqual(is_even(4), True)
-        self.assertEqual(is_even(0), True)
-        self.assertEqual(is_even(-1), False)
+    def test_initialize_database(self):
+        # Test if the database is initialized correctly
+        try:
+            initialize_database()
+            self.assertTrue(True)  # If no exception is raised, the test passes
+        except Exception as e:
+            self.fail(f"initialize_database raised an exception: {e}")
 
 if __name__ == "__main__":
     unittest.main()
